7a7240eee1bc385eb76d6506b0de938ccf3bdeab
kexin-jiang <84614521+kexin-jiang@users.noreply.github.com>
2021-05-23 17:03:11 +0800

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,37 @@
+let name ='🐶' //string
+let age = 100
+let isAdult = true
+let array = []
+let fruits = ['🍌','🍎']
+let name =['hayden','hi','hii']
+names[]
+
+
+let student1 = {}
+student.name = 'hayden'
+student.age = 19
+student.level = 2
+
+
+
+let student2 = {}
+student.name = 'hi'
+student.age = 19
+student.level = 2
+
+
+
+let student3 = {}
+student.name = 'hii'
+student.age = 19
+student.level = 2
+
+
+function showInfo(x){
+    console.log(x.name);
+}
+
+showInfo(student)
+
+
+console.log(student);
